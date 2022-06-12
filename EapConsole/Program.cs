871f66987cbd414c69d5abc444ae3c931133db2c
Fazrin <mfmfazrin1986@gmail.com>
2022-06-12 21:53:10 +0530

    commit

--- EapConsole/Program.cs
+++ EapConsole/Program.cs
@@ -1,3 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+
+
+
+// Keep the console window open in debug mode.
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+
+//create a class to hold employee data 
+public class Employee
+{
+    private long Id { get; set; }
+    private string Name { get; set; }
+    private string Department { get; set; }
+    private string Email { get; set; }
+    private string Phone { get; set; }
+    
+    public Employee(Employee emp)
+    {
+        Id = emp.Id;
+        Name = emp.Name;
+        Department = emp.Department;
+        Email = emp.Email;
+        Phone = emp.Phone;
+    }
+
+    
+}
